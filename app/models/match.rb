f7876ef3fcf7e8c7e416eb333d4292eaa1226961
Jose Piccioni <josepiccioni@gmail.com>
2019-03-07 12:50:16 -0300

    Add match first specs

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,9 @@ class Match < ApplicationRecord
   belongs_to :league
   belongs_to :black_player, class_name: 'Player'
   belongs_to :white_player, class_name: 'Player'
+  validate :not_playing_oneself
+
+  def not_playing_oneself
+    errors.add(:white_player_id, 'Is the same as black player') unless black_player != white_player
+  end
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:black_player) { build :player }
+  let(:white_player) { build :player }
+  let(:location) { build :location }
+  let(:league) { build :league }
+  subject { described_class.new location: location, league: league, black_player: black_player, white_player: white_player  }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a location' do
+    subject.location = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a league' do
+    subject.league = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a white_player' do
+    subject.white_player = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a black_player' do
+    subject.black_player = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid if white and black player are the same' do
+    subject.black_player = subject.white_player
+    expect(subject).not_to be_valid
+  end
 end
