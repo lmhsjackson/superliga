4cd1376081a05bc201d6b04ed18258352c826ec6
Jose Piccioni <josepiccioni@gmail.com>
2019-03-07 13:09:02 -0300

    Add simple specs and email validation for player.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,6 @@
 class Player < ApplicationRecord
+  validates_presence_of :email
+  # TODO: replace when device is in place.
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:player) }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without an email' do
+    subject.email = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without an email' do
+    subject.email = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a correct email format' do
+    subject.email = 'notAmail.com'
+    expect(subject).not_to be_valid
+  end
 end
