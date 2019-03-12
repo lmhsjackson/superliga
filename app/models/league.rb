618944a29b15554406f582689392b36306d212ee
Jose Piccioni <josepiccioni@gmail.com>
2019-03-12 16:51:21 -0300

    Add simple validations to League model + first specs.

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,3 +1,6 @@
 class League < ApplicationRecord
   belongs_to :director, class_name: 'Player'
+  validates :name, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe League, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:league) }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a director' do
+    subject.director = nil
+    expect(subject).not_to be_valid
+  end
 end
