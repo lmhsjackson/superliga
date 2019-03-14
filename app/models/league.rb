f47d549894f492fcb1c800fb2e4160e3458fcc55
Jose Piccioni <josepiccioni@gmail.com>
2019-03-14 17:40:53 -0300

    Add players for league.

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,6 +1,13 @@
 class League < ApplicationRecord
   belongs_to :director, class_name: 'Player'
+  has_many :matches
+  has_many :white_players, through: :matches
+  has_many :black_players, through: :matches
   validates :name, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  def players
+    (black_players + white_players).uniq
+  end
 end
