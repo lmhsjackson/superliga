827c9e498402fabbd1a322d9d1bab5ce86631578
Jose Piccioni <josepiccioni@gmail.com>
2019-03-15 17:28:50 -0300

    Add dependant destroy

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,6 +1,6 @@
 class League < ApplicationRecord
   belongs_to :director, class_name: 'Player'
-  has_many :matches
+  has_many :matches, dependent: :destroy
   has_many :white_players, through: :matches
   has_many :black_players, through: :matches
   validates :name, presence: true
