c45bed92835e9ddc7a3b235e6a5e546370959bdc
Jose Piccioni <josepiccioni@gmail.com>
2019-03-26 14:56:35 -0300

    Cleanup in match and match factory.

--- app/models/match.rb
+++ app/models/match.rb
@@ -7,17 +7,14 @@ class Match < ApplicationRecord
   VICTORY_CONDITIONS = ['resignation', 'time', 'points'].freeze
   WINNER = ['black', 'white'].freeze
 
-  validate :not_playing_oneself
+  validate :two_different_oponents
   validates :victory_condition, inclusion: { in: VICTORY_CONDITIONS, message: "is not valid." }
 
-  def self.by_player(player)
-    # where(black_player_id: player.id).or(where(white_player_id: player.id))
-  end
 
 
   private
 
-  def not_playing_oneself
-    # errors.add(:white_player_id, 'Is the same as black player') unless black_player != white_player
+  def two_different_oponents
+    participants.uniq.count == 2
   end
 end
