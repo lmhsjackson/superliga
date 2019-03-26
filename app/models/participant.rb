21f4cd9bda1bb134a8499c7e5c3c0db3b601bab9
Jose Piccioni <josepiccioni@gmail.com>
2019-03-26 17:12:08 -0300

    Delegate first and last name to player.
      - validates_presence_of score.

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,11 +3,6 @@ class Participant < ApplicationRecord
   belongs_to :league
   has_many :match_participations
   has_many :matches, through: :match_participations
-
-  def self.new_from(player, rank=nil)
-    create(first_name: player.first_name,
-           last_name: player.last_name,
-           rank: (rank || player.rango),
-           score: 0)
-  end
+  delegate :first_name, :last_name, to: :player
+  validates_presence_of :score
 end
