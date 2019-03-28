df55938ad7d3c3b0704aadeea9667678228456e2
Jose Piccioni <josepiccioni@gmail.com>
2019-03-28 18:30:09 -0300

    Initial views and controllers for Participants

---
+++ app/controllers/participants_controller.rb
@@ -0,0 +1,61 @@
+class ParticipantsController < ApplicationController
+  before_action :set_participant, only: [:show, :edit, :update, :destroy]
+  before_action :set_league, only: [:edit, :new]
+
+  # GET /participants
+  # GET /participants.json
+  def index
+    @participants = Participant.all
+  end
+
+  # GET /participants/1
+  # GET /participants/1.json
+  def show
+  end
+
+  # GET /participants/new
+  def new
+    @participant = Participant.new
+  end
+
+  # GET /participants/1/edit
+  def edit
+  end
+
+  # POST /participants
+  # POST /participants.json
+  def create
+    @participant = Participant.new(participant_params)
+    flash[:notice] = "Participant was created successfully." if @participant.save
+    respond_with @participant
+  end
+
+  # PATCH/PUT /participants/1
+  # PATCH/PUT /participants/1.json
+  def update
+    flash[:notice] = "Participant was created successfully." if @participant.update(participant_params)
+    respond_with(@participant)
+  end
+
+  # DELETE /participants/1
+  # DELETE /participants/1.json
+  def destroy
+    @participant.destroy
+    respond_with(@participant)
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_participant
+      @participant = Participant.find(params[:id])
+    end
+
+    def set_league
+      @league = League.find(params[:league_id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def participant_params
+      params.require(:participant).permit(:name, :address)
+    end
+end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,6 +3,6 @@ class Participant < ApplicationRecord
   belongs_to :league
   has_many :match_participations
   has_many :matches, through: :match_participations
-  delegate :first_name, :last_name, to: :player
+  delegate :first_name, :last_name, :full_name, to: :player
   validates_presence_of :score
 end
