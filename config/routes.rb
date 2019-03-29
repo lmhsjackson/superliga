0cecb74962dcd1a7f5fd43b5330c82098f06849b
Jose Piccioni <josepiccioni@gmail.com>
2019-03-29 14:44:39 -0300

    Remove boilerplate added routes for participants.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'participants/index'
-  get 'participants/new'
-  get 'participants/show'
-  get 'participants/create'
-  get 'participants/update'
-  get 'participants/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :players
   resources :leagues do
