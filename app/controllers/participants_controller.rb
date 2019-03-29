15c50c5b7c516ed84c96a9c5695f8dce00bef8a3
Jose Piccioni <josepiccioni@gmail.com>
2019-03-29 14:45:10 -0300

    Fix participant views and controllers

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,6 +1,6 @@
 class ParticipantsController < ApplicationController
   before_action :set_participant, only: [:show, :edit, :update, :destroy]
-  before_action :set_league, only: [:edit, :new]
+  before_action :set_league
 
   # GET /participants
   # GET /participants.json
@@ -10,8 +10,7 @@ class ParticipantsController < ApplicationController
 
   # GET /participants/1
   # GET /participants/1.json
-  def show
-  end
+  def show; end
 
   # GET /participants/new
   def new
@@ -19,43 +18,45 @@ class ParticipantsController < ApplicationController
   end
 
   # GET /participants/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /participants
   # POST /participants.json
   def create
-    @participant = Participant.new(participant_params)
+    @participant = @league.participants.new(participant_params)
     flash[:notice] = "Participant was created successfully." if @participant.save
-    respond_with @participant
+    respond_with(@league, @participant)
   end
 
   # PATCH/PUT /participants/1
   # PATCH/PUT /participants/1.json
   def update
-    flash[:notice] = "Participant was created successfully." if @participant.update(participant_params)
-    respond_with(@participant)
+    flash[:notice] = 'Participant was created successfully.' if @participant.update(participant_params)
+    respond_with(@league, @participant)
   end
 
   # DELETE /participants/1
   # DELETE /participants/1.json
   def destroy
-    @participant.destroy
-    respond_with(@participant)
+   @participant.destroy
+   redirect_to league_participants_path(@league)
+    # respond_with(@league, @participant)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_participant
-      @participant = Participant.find(params[:id])
-    end
 
-    def set_league
-      @league = League.find(params[:league_id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_participant
+    @participant = Participant.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def participant_params
-      params.require(:participant).permit(:name, :address)
-    end
+  def set_league
+    @league = League.find(params[:league_id])
+  end
+
+  # Never trust parameters from the scary internet,
+  # only allow the white list through.
+  def participant_params
+    params.require(:participant).permit(:rank, :player_id, :league_id)
+  end
 end
--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe ParticipantsController, type: :controller do
-
 end
