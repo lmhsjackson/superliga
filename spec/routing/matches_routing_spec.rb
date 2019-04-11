6a05cc96872bd58debd2808fe138ebd0cbed4fea
Jose Piccioni <josepiccioni@gmail.com>
2019-04-11 15:08:00 -0300

    Fixed matches routing specs.
      - Since now matches are nested inside leagues

--- spec/routing/matches_routing_spec.rb
+++ spec/routing/matches_routing_spec.rb
@@ -3,36 +3,36 @@ require "rails_helper"
 RSpec.describe MatchesController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(:get => "/matches").to route_to("matches#index")
+      expect(:get => "leagues/1/matches").to route_to("matches#index", league_id: '1')
     end
 
     it "routes to #new" do
-      expect(:get => "/matches/new").to route_to("matches#new")
+      expect(:get => "leagues/1/matches/new").to route_to("matches#new", league_id: '1')
     end
 
     it "routes to #show" do
-      expect(:get => "/matches/1").to route_to("matches#show", :id => "1")
+      expect(:get => "leagues/1/matches/1").to route_to("matches#show", :id => "1", league_id: '1')
     end
 
     it "routes to #edit" do
-      expect(:get => "/matches/1/edit").to route_to("matches#edit", :id => "1")
+      expect(:get => "leagues/1/matches/1/edit").to route_to("matches#edit", :id => "1", league_id: '1')
     end
 
 
     it "routes to #create" do
-      expect(:post => "/matches").to route_to("matches#create")
+      expect(:post => "leagues/1/matches").to route_to("matches#create", league_id: '1')
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/matches/1").to route_to("matches#update", :id => "1")
+      expect(:put => "leagues/1/matches/1").to route_to("matches#update", :id => "1", league_id: '1')
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/matches/1").to route_to("matches#update", :id => "1")
+      expect(:patch => "leagues/1/matches/1").to route_to("matches#update", :id => "1", league_id: '1')
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/matches/1").to route_to("matches#destroy", :id => "1")
+      expect(:delete => "leagues/1/matches/1").to route_to("matches#destroy", :id => "1", league_id: '1')
     end
   end
 end
