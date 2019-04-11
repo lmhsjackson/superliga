bcea69e01ed7574b616348f97360ac6cfd8743c5
Jose Piccioni <josepiccioni@gmail.com>
2019-04-11 15:00:55 -0300

    Add MatchParticipation first unit specs.

--- spec/factories/match_participation.rb
+++ spec/factories/match_participation.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :match_participation do
-  #   association :match
-  #   association :participant
+    association :match
+    association :participant
+    color { 'black' }
+  end
+
+  trait :white do
+    color { 'white' }
   end
 end
--- spec/models/match_participation_spec.rb
+++ spec/models/match_participation_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe MatchParticipation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:match_participation) }
+
+  describe 'AR record validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a color' do
+      subject.color = nil
+      expect(subject).to be_invalid
+    end
+
+  end
 end
