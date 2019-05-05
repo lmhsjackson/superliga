d6db388ba25f761c2939a2ece900a09737608332
Jose Piccioni <josepiccioni@gmail.com>
2019-05-05 15:40:18 -0300

    League positions table (#7)
    * Add update_score method to participant.
    
    * Match participation calls participant update score on commit.
    
    * Adjust participants view.
    
      - what we called a league table is basically participants index sort
    by score.

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -5,4 +5,12 @@ class Participant < ApplicationRecord
   has_many :matches, through: :match_participations
   delegate :first_name, :last_name, :full_name, to: :player
   validates_presence_of :score
+
+  def update_score
+    new_score = 0
+    match_participations.each do |game|
+      new_score += game.winner ? 3 : 1
+    end
+    update score: new_score
+  end
 end
--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -3,6 +3,29 @@ require 'rails_helper'
 RSpec.describe Participant, type: :model do
   subject { build(:participant) }
 
+  describe '#update_score' do
+    let(:won_games) { build_list(:match_participation, 3, :won) }
+    let(:lost_games) { build_list(:match_participation, 3) }
+
+    it 'adds 3 points per won game' do
+      subject.match_participations << won_games
+      subject.update_score
+      expect(subject.score).to eq(9)
+    end
+
+    it 'adds 1 point per lost game' do
+      subject.match_participations << lost_games
+      subject.update_score
+      expect(subject.score).to eq(3)
+    end
+
+    it 'counts both lost and won games' do
+      subject.match_participations << lost_games + won_games
+      subject.update_score
+      expect(subject.score).to eq(12)
+    end
+  end
+
   describe 'AR record validations' do
     it 'is valid with valid attributes' do
       expect(subject).to be_valid
